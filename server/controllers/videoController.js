bb31eda699ae5d8b5ea4ee049813b0de9212fd7b
Gajendra Sharma <gajendrasharma0145@gmail.com>
2025-07-08 22:38:49 +0530

    Add static FFmpeg with proper codecs for AVI/MP4/MOV

--- server/controllers/videoController.js
+++ server/controllers/videoController.js
@@ -1,23 +1,65 @@
 const ffmpegService = require("../services/ffmpegService");
 const fileService = require("../services/fileService");
+const ffmpeg = require('fluent-ffmpeg');
 
-const convertVideo = async (req, res) => {
-  const { targetFormat, quality } = req.body;
-  
-  try {
-    const outputPath = await ffmpegService.convertVideo(req.file.path, {
-      targetFormat,
-      quality
-    });
+ffmpeg.setFfmpegPath('./bin/ffmpeg');
 
-    res.download(outputPath, `converted.${targetFormat}`, () => {
-      fileService.cleanupFiles([req.file.path, outputPath]);
-    });
-  } catch (err) {
-    console.error("Video conversion error:", err);
-    res.status(500).send("Video conversion failed");
-  }
-};
+async function convertVideo(inputPath, { targetFormat, quality }) {
+  const outputPath = inputPath.replace(/\.\w+$/, `.${targetFormat}`);
+
+  return new Promise((resolve, reject) => {
+    let command = ffmpeg(inputPath).output(outputPath);
+
+    // Set bitrate based on quality
+    switch (quality) {
+      case 'high':
+        command.videoBitrate('2000k');
+        break;
+      case 'medium':
+        command.videoBitrate('1000k');
+        break;
+      case 'low':
+        command.videoBitrate('500k');
+        break;
+    }
+
+    // Set codecs based on target format
+    switch (targetFormat) {
+      case 'avi':
+        command
+          .videoCodec('mpeg4')          // For AVI video
+          .audioCodec('libmp3lame');    // For AVI audio
+        break;
+      case 'mp4':
+        command
+          .videoCodec('libx264')
+          .audioCodec('aac');
+        break;
+      case 'mov':
+        command
+          .videoCodec('libx264')
+          .audioCodec('aac');
+        break;
+      case 'wmv':
+        command
+          .videoCodec('wmv2')
+          .audioCodec('wmav2');
+        break;
+      case 'mkv':
+        command
+          .videoCodec('libx264')
+          .audioCodec('aac');
+        break;
+      default:
+        return reject(new Error('Unsupported format'));
+    }
+
+    command
+      .on('end', () => resolve(outputPath))
+      .on('error', reject)
+      .run();
+  });
+}
 
 const extractAudioFromVideo = async (req, res) => {
   const { targetFormat } = req.body;
